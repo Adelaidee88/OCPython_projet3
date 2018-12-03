f86acd80ca9afac1f01ae6864d33f657a868d524
Adelaide Euriat <adelaide.euriat@gmail.com>
2018-12-03 14:30:02 +0100

    Jeu fonctionnel, manque finalisation

--- labyrinthe.py
+++ labyrinthe.py
@@ -9,11 +9,11 @@ from murdock import Murdock
 from map import Map
 
 
-laby = open("maze.txt", "r")  # ouverture du fichier maze.txt
-contenu = laby.read()  # renvoie le contenu du fichier
+laby = open("maze.txt", "r")  # open file maze.txt
+contenu = laby.read()  # return file content
 
-maze = []  # création de la liste finale du laby (= list_valid)
-maze_list = []  # création de la liste de liste de ligne (= map)
+maze = []  # création de la liste finale du laby
+maze_list = []  # création de la liste de liste de ligne
 maze_str = contenu.splitlines()  # séparation de chaque ligne du fichier en liste de chaines de caractères
 
 
@@ -42,11 +42,10 @@ def way():
 
 pygame.init()
 
-windowSurface = pygame.display.set_mode((600, 600), 0, 32)  # fenêtre de 600x600 px
-pygame.display.set_caption('Labyrinthe')  # nommée Labyrinthe
+windowSurface = pygame.display.set_mode((600, 600), 0, 32)  # window of 600x600 px
+pygame.display.set_caption('Labyrinth')  # named Labyrinth
 
-# définition des couleurs
-
+# colors definition
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -54,29 +53,27 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-# importation des images
-img_mur = pygame.image.load("wall.png")
-img_sol = pygame.image.load("ground.png")
-img_murd = pygame.image.load("murd.png")
-img_macgyver = pygame.image.load("macg.png")
-img_needle = pygame.image.load("needle.png")
-inv_needle = pygame.image.load("needle_loot.png")
-img_tube = pygame.image.load("tube.png")
-inv_tube = pygame.image.load("tube_loot.png")
-img_ether = pygame.image.load("ether.png")
-inv_ether = pygame.image.load("ether_loot.png")
-screen_win = pygame.image.load("screen_win.png")
-screen_lose = pygame.image.load("screen_lose.png")
+# images loaded
+img_mur = pygame.image.load("resources/wall.png")
+img_sol = pygame.image.load("resources/ground.png")
+img_murd = pygame.image.load("resources/murd.png")
+img_macgyver = pygame.image.load("resources/macg.png")
+img_needle = pygame.image.load("resources/needle.png")
+inv_needle = pygame.image.load("resources/needle_loot.png")
+img_tube = pygame.image.load("resources/tube.png")
+inv_tube = pygame.image.load("resources/tube_loot.png")
+img_ether = pygame.image.load("resources/ether.png")
+inv_ether = pygame.image.load("resources/ether_loot.png")
+screen_win = pygame.image.load("resources/screen_win.png")
+screen_lose = pygame.image.load("resources/screen_lose.png")
 
-pygame.mixer.music.load("bonk.wav")  # importation du son de collision
-
-murdock = Murdock(520, 560, "murdock") # création de l'objet Murdock
+pygame.mixer.music.load("resources/bonk.wav")  # collision sound loaded
 
 
 def draw_laby():
-    """Draws the labyrinth from the list created from maze.txt"""
+    """Draws the labyrinth from the maze list created from maze.txt"""
     i = 0
-    while i < 600:  # boucle de couleur des cases selon valeur name de maze
+    while i < 600:  # cases texture from name value of maze
         j = 0
         while j < 600:
             case = Square(i // 40, j // 40, maze[i // 40][j // 40])
@@ -94,79 +91,84 @@ def draw_laby():
         i = i + 40
 
 
-pos_macgyver = Macgyver(520, 40, "m")  # création de l'objet MacG
+macgyver = Macgyver(520, 40, "m")  # création de l'objet MacG
+murdock = Murdock(520, 560, "murdock") # création de l'objet Murdock
 
-objets = random.sample(way(), 3)  # prend 3 positions dans la liste de cases vides créée par la fonction way()
+objects = random.sample(way(), 3)  # prend 3 positions dans la liste de cases vides créée par la fonction way()
 
-needle = objets[0]  # l'aiguille est le premier objet de la liste random
+needle = objects[0]  # l'aiguille est le premier objet de la liste random
 pos_needle = Loot(needle.x*40, needle.y*40, "needle")  # création de l'objet needle
 
-tube = objets[1]  # le tube est le deuxième objet de la liste random
+tube = objects[1]  # le tube est le deuxième objet de la liste random
 pos_tube = Loot(tube.x*40, tube.y*40, "tube")  # création de l'objet tube
 
-ether = objets[2]  # l'ether est le troisième objet de la liste random
+ether = objects[2]  # l'ether est le troisième objet de la liste random
 pos_ether = Loot(ether.x*40, ether.y*40, "ether")  # création de l'objet ether
 
 win = False
 lose = False
+end = False
 
 while 1:
     for event in pygame.event.get():
 
         # déplacement
-        if event.type == pygame.KEYDOWN :
-            if event.key == pygame.K_LEFT:  # si flèche gauche
-                if maze[pos_macgyver.x//40 - 1][pos_macgyver.y//40] == " ":
-                    pos_macgyver.x = pos_macgyver.x - 40  # se déplace vers la gauche
+        if end == False:
+            if event.type == pygame.KEYDOWN :
+                if event.key == pygame.K_LEFT:  # si flèche gauche
+                    if maze[macgyver.x//40 - 1][macgyver.y//40] == " ":
+                        macgyver.x = macgyver.x - 40  # se déplace vers la gauche
+                    else:
+                        pygame.mixer.music.play()
+                elif event.key == pygame.K_UP:  # si flèche haut
+                    if maze[macgyver.x//40][macgyver.y//40 - 1] == " ":
+                        macgyver.y = macgyver.y - 40  # se déplace vers le haut
+                    else:
+                        pygame.mixer.music.play()
+                elif event.key == pygame.K_RIGHT:  # si flèche droite
+                    if maze[macgyver.x//40 + 1][macgyver.y//40] == " ":
+                        macgyver.x = macgyver.x + 40  # se déplace vers la droite
+                    else:
+                        pygame.mixer.music.play()
+                elif event.key == pygame.K_DOWN:  # si flèche bas
+                    if maze[macgyver.x//40][macgyver.y//40 + 1] == " " or maze[macgyver.x//40][macgyver.y//40 + 1] == "v":
+                        macgyver.y = macgyver.y + 40  # se déplace vers le bas
+                    else:
+                        pygame.mixer.music.play()  # joue le bruit de collision
                 else:
-                    pygame.mixer.music.play()
-            elif event.key == pygame.K_UP:  # si flèche haut
-                if maze[pos_macgyver.x//40][pos_macgyver.y//40 - 1] == " ":
-                    pos_macgyver.y = pos_macgyver.y - 40  # se déplace vers le haut
-                else:
-                    pygame.mixer.music.play()
-            elif event.key == pygame.K_RIGHT:  # si flèche droite
-                if maze[pos_macgyver.x//40 + 1][pos_macgyver.y//40] == " ":
-                    pos_macgyver.x = pos_macgyver.x + 40  # se déplace vers la droite
-                else:
-                    pygame.mixer.music.play()
-            elif event.key == pygame.K_DOWN:  # si flèche bas
-                if maze[pos_macgyver.x//40][pos_macgyver.y//40 + 1] == " " or maze[pos_macgyver.x//40][pos_macgyver.y//40 + 1] == "v":
-                    pos_macgyver.y = pos_macgyver.y + 40  # se déplace vers le bas
-                else:
-                    pygame.mixer.music.play()  # joue le bruit de collision
-            else:
-                pass
+                    pass
 
-        # loot des objets
-        if pos_macgyver.x == pos_needle.x and pos_macgyver.y == pos_needle.y:  # si MacG arrive sur l'aiguille
-            pos_needle.picked = True
-            pos_macgyver.loot[0] = True
-        if pos_macgyver.x == pos_tube.x and pos_macgyver.y == pos_tube.y:  # si MacG arrive sur le tube
-            pos_tube.picked = True
-            pos_macgyver.loot[1] = True
-        if pos_macgyver.x == pos_ether.x and pos_macgyver.y == pos_ether.y:  # si MacG arrive sur l'ether
-            pos_ether.picked = True
-            pos_macgyver.loot[2] = True
+            # loot des objets
+            if macgyver.x == pos_needle.x and macgyver.y == pos_needle.y:  # si MacG arrive sur l'aiguille
+                pos_needle.picked = True
+                macgyver.loot[0] = True
+            if macgyver.x == pos_tube.x and macgyver.y == pos_tube.y:  # si MacG arrive sur le tube
+                pos_tube.picked = True
+                macgyver.loot[1] = True
+            if macgyver.x == pos_ether.x and macgyver.y == pos_ether.y:  # si MacG arrive sur l'ether
+                pos_ether.picked = True
+                macgyver.loot[2] = True
 
         # fin du jeu
-        if pos_macgyver.x == murdock.x and pos_macgyver.y == murdock.y:  # MacG arrive sur Murdock
-            if pos_macgyver.loot[0] and pos_macgyver.loot[1] and pos_macgyver.loot[2]:
+        if macgyver.x == murdock.x and macgyver.y == murdock.y:  # MacG arrive sur Murdock
+            end = True
+            if macgyver.loot[0] and macgyver.loot[1] and macgyver.loot[2]:
                 win = True
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_c:  # relance le jeu
                         # regrouper toutes ces conditions dans un truc "conditions initiales"
                         win = False
                         lose = False
+                        end = False
                         pos_needle.picked = False
-                        pos_macgyver.loot[0] = False
+                        macgyver.loot[0] = False
                         pos_tube.picked = False
-                        pos_macgyver.loot[1] = False
+                        macgyver.loot[1] = False
                         pos_ether.picked = False
-                        pos_macgyver.loot[2] = False
-                        pos_macgyver = Macgyver(520, 40, "m")
+                        macgyver.loot[2] = False
+                        macgyver = Macgyver(520, 40, "m")
                         # coordonnées aléatoires des loots
-                    if event.key == pygame.K_a:
+                    if event.key == pygame.K_ESCAPE:
                         sys.exit()
                     else:
                         pass
@@ -177,26 +179,26 @@ while 1:
                         # regrouper toutes ces conditions dans un truc "conditions initiales"
                         win = False
                         lose = False
+                        end = False
                         pos_needle.picked = False
-                        pos_macgyver.loot[0] = False
+                        macgyver.loot[0] = False
                         pos_tube.picked = False
-                        pos_macgyver.loot[1] = False
+                        macgyver.loot[1] = False
                         pos_ether.picked = False
-                        pos_macgyver.loot[2] = False
-                        pos_macgyver = Macgyver(520, 40, "m")
+                        macgyver.loot[2] = False
+                        macgyver = Macgyver(520, 40, "m")
                         # coordonnées aléatoires des loots
-                    if event.key == pygame.K_a:
+                    if event.key == pygame.K_ESCAPE:
                         sys.exit()
                     else:
                         pass
         if event.type == pygame.QUIT:
             sys.exit()
 
-    # commencer cours P4 ou revoir code
-    # préparer papier de soutenance
 
-    draw_laby()
-    windowSurface.blit(img_macgyver, (pos_macgyver.x, pos_macgyver.y))
+    # affichage
+    draw_laby()  # affiche le labyrinthe
+    windowSurface.blit(img_macgyver, (macgyver.x, macgyver.y))  # affiche MacGyver
     if pos_needle.picked == False:
         windowSurface.blit(img_needle, (pos_needle.x, pos_needle.y))  # affiche l'aiguille dans le labyrinthe
     else:
